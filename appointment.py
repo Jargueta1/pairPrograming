8052efc3d4ab05241242897721eb0b23cf44d3df
Jorge Argueta <67116860+Jargueta1@users.noreply.github.com>
2021-10-08 06:51:06 -0400

    Initialize file and docstring

---
+++ appointment.py
@@ -0,0 +1,33 @@
+"""Exercise 5 for INST 326 
+Driver: David Mejias
+Navigator: Jorge Argueta
+Assignment: Exercise 5
+Date: 10/8/2021
+Challenges Encountered:  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+"""
+
+class Appointment:
+
+    def __init__(self):
+        """Creats attributes of the object and sets infomation based on paramenters 
+        Args:        
+            name(str): Indicates the name of the appointment 
+            start(tuple): sets the start time of appointment consists of two int where 1st is hours (24-format) and 2nd is minutes
+            end (tuple): sets the end time of appointment consists of two int where 1st is hours (24-format) and 2nd is minutes
+    
+        Returns: 
+                Test
+                None
+        """
+        pass
+
+    def overlap(sef):
+        """Determines wether two appointments overlap  
+        Args:        
+            other (Appointment): Other appointment object 
+    
+        Returns:        
+                Boolean 
+        """
+        pass
+        pass
